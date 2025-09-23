0e29acaf784d6dc1596871df5a3203bf3524d6b1
Mehreen Israr <mehreenisrar.26@gmail.com>
2025-09-23 12:11:27 +0500

    removed unnecessary logos from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -112,19 +112,7 @@ const Header = ({ onLogout }) => {
         {/* Right Section */}
         <div className="header-right">
           {/* Quick Actions */}
-          <div className="header-actions">
-            <button className="action-btn">
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"/>
-              </svg>
-            </button>
-            
-            <button className="action-btn">
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
-              </svg>
-            </button>
-          </div>
+          
 
           {/* Notifications */}
           <div className="header-action notification-container">
